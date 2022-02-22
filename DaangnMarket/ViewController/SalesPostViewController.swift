f3abac352b217c45ef213880ba0c9869ed034df3
DongHee-Sin <voslr0601@naver.com>
2022-02-22 11:27:21 +0900

    1. 판매글 UI 컬렉션뷰만 제외하고 완성 2. 전달받은 글 정보를 바탕으로 UI 업데이트 구현

--- DaangnMarket/ViewController/SalesPostViewController.swift
+++ DaangnMarket/ViewController/SalesPostViewController.swift
@@ -16,17 +16,69 @@ class SalesPostViewController: MainViewController {
         self.navigationController?.popViewController(animated: true)
     }
     
-    // UI 연결
+    // MARK: - UI 연결
+    // 채팅거는 버튼
     @IBOutlet weak var toChattingButton: UIButton!
     
+    // 상품 이미지
+    @IBOutlet weak var productImage: UIImageView!
+    
+    // 사용자 이름과 주소
+    @IBOutlet weak var nickName: UILabel!
+    @IBOutlet weak var address: UILabel!
+    
+    // 매너온도 관련
+    @IBOutlet weak var mannerScore: UILabel!
+    @IBOutlet weak var mannerFace: UIImageView!
+    @IBOutlet weak var temperature: UIImageView!
+    @IBOutlet weak var mannerEnergy: UIImageView!
+    
+    // 제목 / 카테고리 / 내용
+    @IBOutlet weak var postTitle: UILabel!
+    @IBOutlet weak var postCategory: UILabel!
+    @IBOutlet weak var postContent: UILabel!
+    
+    // 채팅 / 관심 / 조회
+    @IBOutlet weak var chattingInterestingChecking: UILabel!
     
     
+    // MARK: - UI 업데이트 함수
+    func updatePost(_ data: SalesPost) {
+        if data.imageString == "기본이미지.jpeg" {
+            productImage.isHidden = true
+        }else {
+            productImage.image = UIImage(named: data.imageString)
+        }
+        nickName.text = data.writer.nickName
+        address.text = data.writer.address
+        if data.writer.mannerScore >= 40 {
+            mannerScore.text = String(data.writer.mannerScore)
+            mannerScore.textColor = UIColor(red: 255/255, green: 173/255, blue: 57/255, alpha: 1)
+            setMannerImage("노란")
+        }else {
+            mannerScore.text = String(data.writer.mannerScore)
+            mannerScore.textColor = UIColor(red: 15/255, green: 96/255, blue: 163/255, alpha: 1)
+            setMannerImage("파란")
+        }
+        postTitle.text = data.title
+        postCategory.text = data.category.rawValue
+        postContent.text = data.content
+    }
+    
+    // 온도 이미지 업데이트 함수
+    func setMannerImage(_ color: String) {
+        mannerFace.image = UIImage(named: "\(color)얼굴.jpeg")
+        temperature.image = UIImage(named: "\(color)온도.png")
+        mannerEnergy.image = UIImage(named: "\(color)에너지.png")
+    }
     
     
-    
+    // 글 정보 받아오기
     var receivedData: SalesPost?
     
     
+    
+    // MARK: - View Did Load
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -35,6 +87,6 @@ class SalesPostViewController: MainViewController {
         
         
         // UI 업데이트
-        
+        updatePost(receivedData!)
     }
 }
