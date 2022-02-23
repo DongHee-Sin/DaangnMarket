aedda009c3a2a71884e1d3be2379a503f47eb41c
DongHee-Sin <voslr0601@naver.com>
2022-02-23 19:51:36 +0900

    동네생활 ViewController 생성

---
+++ DaangnMarket/TableViewCell/VillageLifeTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  VillageLifeTableViewCell.swift
+//  DaangnMarket
+//
+//  Created by 신동희 on 2022/02/23.
+//
+
+import UIKit
+
+class VillageLifeTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
---
+++ DaangnMarket/ViewController/VillageLifeViewController.swift
@@ -0,0 +1,25 @@
+//
+//  VillageLifeViewController.swift
+//  DaangnMarket
+//
+//  Created by 신동희 on 2022/02/23.
+//
+
+import Foundation
+import UIKit
+
+class VillageLifeViewController: MainViewController {
+    
+    // MARK: - TableView 연결
+    @IBOutlet weak var tableView: UITableView!
+    
+    
+    
+    
+    // MARK: - View Did Load
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
+
